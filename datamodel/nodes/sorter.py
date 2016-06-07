4f70482c0ccfda3935144244735933b76224182d
csparpa <csparpa@gmail.com>
2016-06-08 00:36:56 +0200

    basic sorter node

---
+++ datamodel/nodes/sorter.py
@@ -0,0 +1,28 @@
+import node
+
+
+class Sorter(node.Node):
+
+    def __init__(self, sorting_function=None, name=None):
+        node.Node.__init__(self, name=name)
+        self._sorting_function = sorting_function
+
+    # Righthand
+    def input(self, unsorted):
+        self._context = unsorted
+
+    # Lefthand
+    def output(self):
+        if self._sorting_function is None:
+            return sorted(self._context)
+        return self._sorting_function(self._context)
+
+
+class ReverseSorter(Sorter):
+
+    # Lefthand
+    def output(self):
+        if self._sorting_function is None:
+            return sorted(self._context, reverse=True)
+        return self._sorting_function(self._context)
+
