243d09c0826cee3c268dfba2326bb72c19a6ccee
csparpa <csparpa@gmail.com>
2016-06-14 22:51:03 +0200

    Better imports

--- datamodel/nodes/quick/maths.py
+++ datamodel/nodes/quick/maths.py
@@ -1,86 +1,86 @@
 import math
-
 from datamodel.lib.node import Node
-from ..apply import ApplyStatic
-from ..value import Value
+from datamodel.nodes import apply, value
 
 
 # Constants
-class Pi(Value):
+class Pi(value.Value):
     def __init__(self, name=None):
-        Value.__init__(self, 3.14159265358979323846264338327950288419716939937,
-                       name=name)
+        value.Value.__init__(self,
+                             3.14159265358979323846264338327950288419716939937,
+                             name=name)
 
 
-class E(Value):
+class E(value.Value):
     def __init__(self, name=None):
-        Value.__init__(self, 2.71828182845904523536028747135266249775724709369,
-                       name=name)
+        value.Value.__init__(self,
+                             2.71828182845904523536028747135266249775724709369,
+                             name=name)
 
 
 # Algebraic
 
-class Sum(ApplyStatic):
+class Sum(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, lambda c: sum(c), name=name)
+        apply.ApplyStatic.__init__(self, lambda c: sum(c), name=name)
 
 
-class Product(ApplyStatic):
+class Product(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self,
-                             lambda c: reduce(lambda x, y: x * y, c),
-                             name=name)
+        apply.ApplyStatic.__init__(self,
+                                   lambda c: reduce(lambda x, y: x * y, c),
+                                   name=name)
 
 
-class Floor(ApplyStatic):
+class Floor(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.floor, name=name)
+        apply.ApplyStatic.__init__(self, math.floor, name=name)
 
 
-class Ceil(ApplyStatic):
+class Ceil(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.ceil, name=name)
+        apply.ApplyStatic.__init__(self, math.ceil, name=name)
 
 
-class Sqrt(ApplyStatic):
+class Sqrt(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.sqrt, name=name)
+        apply.ApplyStatic.__init__(self, math.sqrt, name=name)
 
 
 # Extrema
 
-class Max(ApplyStatic):
+class Max(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, max, name=name)
+        apply.ApplyStatic.__init__(self, max, name=name)
 
 
-class Min(ApplyStatic):
+class Min(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, min, name=name)
+        apply.ApplyStatic.__init__(self, min, name=name)
 
 
 # Trigonometrical
 
-class Sin(ApplyStatic):
+class Sin(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.sin, name=name)
+        apply.ApplyStatic.__init__(self, math.sin, name=name)
 
 
-class Cos(ApplyStatic):
+class Cos(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.cos, name=name)
+        apply.ApplyStatic.__init__(self, math.cos, name=name)
 
 
-class Abs(ApplyStatic):
+class Abs(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.fabs, name=name)
+        apply.ApplyStatic.__init__(self, math.fabs, name=name)
 
 
 # Exponentations
 
-class Exp(ApplyStatic):
+class Exp(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.exp, name=name)
+        apply.ApplyStatic.__init__(self, math.exp, name=name)
 
 
 class Power(Node):
@@ -96,12 +96,12 @@ class Power(Node):
 
 
 # Logs
-class Log(ApplyStatic):
+class Log(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.log, name=name)
+        apply.ApplyStatic.__init__(self, math.log, name=name)
 
 
-class Log10(ApplyStatic):
+class Log10(apply.ApplyStatic):
     def __init__(self, name=None):
-        ApplyStatic.__init__(self, math.log10, name=name)
+        apply.ApplyStatic.__init__(self, math.log10, name=name)
 
