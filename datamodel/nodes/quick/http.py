396cb49e4e18a295ea3c953f45ece12a582adcf5
Claudio Sparpaglione <claudio@walletsaver.com>
2016-06-15 15:44:40 +0200

    http node for handling HTTP calls (only GET for now), leveraging Requests as networking library; new sample graph also

---
+++ datamodel/nodes/quick/http.py
@@ -0,0 +1,62 @@
+import requests
+from datamodel.base import node
+
+
+class HttpClient(node.Node):
+
+    DEFAULT_TIMEOUT_SECS = 3
+
+    def __init__(self, name=None):
+        node.Node.__init__(self, name=name)
+
+    def input(self, request_params):
+        self._url = request_params.get('url', None)
+        self._headers = request_params.get('headers', dict())
+        self._auth = request_params.get('auth', tuple())
+        self._output_encoding = request_params.get('output_encoding', 'json')
+        self._verify_ssl = request_params.get('verify_ssl', True)
+        self._timeout_secs = request_params.get('timeout_secs',
+                                                self.DEFAULT_TIMEOUT_SECS)
+
+    def output(self):
+        pass
+
+    def reset(self):
+        del self._url
+        del self._headers
+        del self._auth
+        del self._output_encoding
+        del self._verify_ssl
+        del self._timeout_secs
+
+
+class RawGet(HttpClient):
+
+    def input(self, request_params):
+        self._query = request_params.get('query', dict())
+        HttpClient.input(self, request_params)
+
+    def output(self):
+        r = requests.get(self._url, auth=self._auth, params=self._query,
+                         headers=self._headers, timeout=self._timeout_secs,
+                         verify=self._verify_ssl)
+        payload = self._encode_payload(r)
+        return r.status_code, payload, r
+
+    def _encode_payload(self, response):
+        if self._output_encoding == 'json':
+            return response.json()
+        if self._output_encoding == 'binary':
+            return response.content
+        return response.text
+
+    def reset(self):
+        del self._query
+        HttpClient.reset(self)
+
+
+class Get(RawGet):
+
+    def output(self):
+        status_code, payload, raw_response = RawGet.output(self)
+        return payload
--- testrun.py
+++ testrun.py
@@ -1,4 +1,5 @@
-from sample_graphs import sum_and_product, sort_and_unique, replace_word
+from sample_graphs import sum_and_product, sort_and_unique, replace_word, \
+    scrape_image
 
 '''
 g1 = sum_and_product.sum_and_product([1, 2, 3, 4])
@@ -8,10 +9,15 @@ output = g1.execute()
 g2 = sort_and_unique.sort_and_unique('sample_graphs/testinput.txt',
                                     'sample_graphs/testoutput.txt')
 g2.execute()
-'''
+
 
 
 g3 = replace_word.replace_word('hello there my name is hello')
 g3.execute()
 g3 = replace_word.replace_word('the word you are looking for is not here')
 g3.execute()
+'''
+
+g4 = scrape_image.scraper_image('https://httpbin.org/image/png',
+                                'sample_graphs/testscraped.png')
+g4.execute()
