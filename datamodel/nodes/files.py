0eb7ab7ba4ef4c39458f14f77d6d9af4e30e6ccc
Claudio Sparpaglione <claudio@walletsaver.com>
2016-06-15 15:43:41 +0200

    Binary file writer (eg. writer for images)

--- datamodel/nodes/files.py
+++ datamodel/nodes/files.py
@@ -35,3 +35,20 @@ class FileWriter(node.Node):
 
     def reset(self):
         del self._context
+
+
+class BinaryFileWriter(node.Node):
+
+    def __init__(self, filepath, name=None):
+        node.Node.__init__(self, name=name)
+        self._filepath = filepath
+
+    def input(self, context):
+        self._context = context
+
+    def output(self):
+        with open(self._filepath, "wb") as bf:
+            bf.write(bytearray(self._context))
+
+    def reset(self):
+        del self._context
