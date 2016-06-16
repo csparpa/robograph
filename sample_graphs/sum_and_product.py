08fcfe57242c16ea87a7e43649945e67ba2ae7fa
Claudio Sparpaglione <claudio@walletsaver.com>
2016-06-16 18:08:10 +0200

    New sample graph

--- sample_graphs/sum_and_product.py
+++ sample_graphs/sum_and_product.py
@@ -1,8 +1,8 @@
 # Given a list of numbers, calculate its sum and product and print it on screen
 
+import logging
 from datamodel.base import graph
 from datamodel.nodes import printer, value, filter
-
 from datamodel.nodes import apply
 
 
@@ -14,6 +14,26 @@ def sum_and_product(list_of_numbers):
     p = printer.ConsolePrinter()
 
     g = graph.Graph('sum_and_product', [v, s, m, c, p])
+
+    g.connect(p, c)
+    g.connect(c, m)
+    g.connect(c, s)
+    g.connect(s, v)
+    g.connect(m, v)
+
+    return g
+
+
+def logged_sum_and_product(list_of_numbers):
+    v = value.Value(list_of_numbers)
+    s = apply.ApplyStatic(lambda c: sum(c))
+    m = apply.ApplyStatic(lambda c: reduce(lambda x, y: x * y, c))
+    c = filter.InputCombiner()
+
+    logging.basicConfig(level=logging.DEBUG)
+    p = printer.LogPrinter(logging.getLogger(__name__), logging.DEBUG)
+
+    g = graph.Graph('logged_sum_and_product', [v, s, m, c, p])
 
     g.connect(p, c)
     g.connect(c, m)
