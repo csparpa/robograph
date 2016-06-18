f53095f364805611c50339dc3e7aaee21e3d29d4
csparpa <csparpa@gmail.com>
2016-06-18 20:31:05 +0200

    ToCSV

--- datamodel/nodes/transcoders.py
+++ datamodel/nodes/transcoders.py
@@ -1,4 +1,5 @@
 import json
+import os
 from datamodel.base import node
 
 
@@ -12,3 +13,27 @@ class ToJSON(node.Node):
 
     def reset(self):
         del self._context
+
+
+class ToCSV(node.Node):
+
+    def __init__(self, header_list=None, delimiter=',', linesep=os.linesep,
+                 name=None):
+        node.Node.__init__(self, name=name)
+        self._header_list = header_list
+        self._delimiter = delimiter
+        self._linesep = linesep
+
+    def input(self, data_matrix):
+        self._data_matrix = data_matrix
+
+    def output(self):
+        lines = self._linesep.join([self._delimiter.join(row) for row in self._data_matrix])
+        if self._header_list:
+            header = self._delimiter.join(self._header_list)
+        else:
+            header = ''
+        return header + self._linesep + lines
+
+    def reset(self):
+        del self._data_matrix
