dc3ae7b39cbe700615b0a8c2196e7dbad39eab45
csparpa <csparpa@gmail.com>
2016-06-19 16:32:40 +0200

    New way: now every node knows about its required parameters

--- datamodel/base/node.py
+++ datamodel/base/node.py
@@ -1,20 +1,66 @@
 class Node:
 
-    def __init__(self, name=None):
-        self._name = name
+    _name = None
+    _reqs = None
+    _params = None
+
+    def __init__(self, **args):
+        self._name = args.get('name', None)
+        self._params = dict()
+        for key in self._reqs:
+            self._params[key] = args.get(key, None)
 
     @property
     def name(self):
+        """
+        The name of this node
+        :return: str
+        """
         return self._name
 
+    @property
+    def requirements(self):
+        """
+        Explains the parameters required by this node
+        :return: list
+        """
+        return self._reqs
+
+    @property
+    def parameters(self):
+        """
+        Gives the state of this node in terms of parameters
+        :return: dict
+        """
+        return self._params
+
     def input(self, context):
-        pass
+        """
+        Updates _params with the _reqs entries that are found into context
+        :param context: dict
+        :return: None
+        """
+        for key in self._reqs:
+            if context.get(key, None):
+                self._params[key] = context[key]
 
     def output(self):
+        """
+        Performs the magic!
+        :return: a value
+        """
         pass
 
     def reset(self):
-        pass
+        """
+        Resets the node's parameters
+        :return:
+        """
+        for key in self._params:
+            self._params[key] = None
 
     def __str__(self):
-        return u'<Node: %s - %s>' % (self._name, unicode(self.__class__))
+        return u'<Node: %s - requires: %s - class: %s>' % (
+            self._name,
+            unicode(','.join(self._reqs)),
+            unicode(self.__class__))
