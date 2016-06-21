dd58566b40f03ecd03918e2753e34a0c2e1cee3e
csparpa <csparpa@gmail.com>
2016-06-21 22:15:25 +0200

    Fix and unit tests for logics

--- datamodel/nodes/quick/logics.py
+++ datamodel/nodes/quick/logics.py
@@ -1,41 +1,63 @@
 from datamodel.nodes import apply
 
 
-class Not(apply.ApplyStatic):
-    def __init__(self, name=None):
-        apply.ApplyStatic.__init__(
+class Not(apply.Apply):
+    """
+    This node negates the truth value of the items of an argument sequence
+    """
+    def __init__(self, **args):
+        apply.Apply.__init__(
             self,
-            lambda predicate: not predicate,
-            name=name)
+            function=lambda predicates: map(lambda x: not x, predicates),
+            name=args.get('name', None))
 
 
-class And(apply.ApplyStatic):
-    def __init__(self, name=None):
-        apply.ApplyStatic.__init__(
+class And(apply.Apply):
+    """
+    This node performs a logical AND on the items of an argument sequence
+    """
+    def __init__(self, **args):
+        apply.Apply.__init__(
             self,
-            lambda predicates: reduce(lambda p1, p2: p1 and p2, predicates),
-            name=name)
+            function=lambda predicates: reduce(lambda p1, p2: p1 and p2, predicates),
+            name=args.get('name', None))
 
 
-class Or(apply.ApplyStatic):
-    def __init__(self, name=None):
-        apply.ApplyStatic.__init__(
+class Or(apply.Apply):
+    """
+    This node performs a logical OR on the items of an argument sequence
+    """
+    def __init__(self, **args):
+        apply.Apply.__init__(
             self,
-            lambda predicates: reduce(lambda p1, p2: p1 or p2, predicates),
-            name=name)
+            function=lambda predicates: reduce(lambda p1, p2: p1 or p2, predicates),
+            name=args.get('name', None))
 
 
-class All(apply.ApplyStatic):
-    def __init__(self, name=None):
-        apply.ApplyStatic.__init__(self, all, name=name)
+class All(apply.Apply):
+    """
+    This node tells if all the items of the argument sequence have a truth value
+    of true
+    """
+    def __init__(self, **args):
+        apply.Apply.__init__(self, function=all, name=args.get('name', None))
 
 
-class Nil(apply.ApplyStatic):
-    def __init__(self, name=None):
-        apply.ApplyStatic.__init__(self, lambda predicates: not any(predicates),
-                                   name=name)
+class Nil(apply.Apply):
+    """
+    This node tells if all the items of the argument sequence have a truth value
+    of false
+    """
+    def __init__(self, **args):
+        apply.Apply.__init__(self,
+                             function=lambda predicates: not any(predicates),
+                             name=args.get('name', None))
 
 
-class Any(apply.ApplyStatic):
-    def __init__(self, name=None):
-        apply.ApplyStatic.__init__(self, any, name=name)
+class Any(apply.Apply):
+    """
+    This node tells if at least one item in the argument sequence has a truth value
+    of true
+    """
+    def __init__(self, **args):
+        apply.Apply.__init__(self, function=any, name=args.get('name', None))
