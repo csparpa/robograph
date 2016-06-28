5bac58d7690a822077ca346c3113d77b1e283f6b
Claudio Sparpaglione <claudio@walletsaver.com>
2016-06-28 15:08:59 +0200

    Template strings

---
+++ datamodel/nodes/strings.py
@@ -0,0 +1,17 @@
+from datamodel.base import node
+
+
+class TemplatedString(node.Node):
+    """
+    This node returns a string by filling a string template with named parameters
+    Requirements:
+      template --> a str template, containing named labels. Format is
+      parameters --> dict with named parameters to be replaced into the template
+    Eg:
+      TemplatedString(template='After {p1} comes {p2}', parameters=dict(p1='one', p2='two'))
+    """
+
+    _reqs = ['template', 'parameters']
+
+    def output(self):
+        return self._params['template'].format(**self._params['parameters'])
